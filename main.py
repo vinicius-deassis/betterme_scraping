5b3282944aa8015df0c96913cf7f37effbf10f69
Vinicius Assis <vassisprof@gmail.com>
2026-06-15 18:26:23 -0300

    fix for selenium on ubuntu server

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
 import telebot
 from dotenv import load_dotenv
 import os
@@ -11,14 +12,22 @@ CHAT_ID = os.environ.get("CHAT_ID")
 bot = telebot.TeleBot(TOKEN)
 
 def is_PS26_2():
-    driver = webdriver.Chrome()
+    # Chrome settings on selenium
+    chrome_options = Options()
+    chrome_options.add_argument('--headless')
+    chrome_options.add_argument('--no-sandbox')
+    chrome_options.add_argument('--disable-dev-shm-usage')
+
+    driver = webdriver.Chrome(chrome_options=chrome_options)
     driver.get("http://selecao.ufrpe.br")
     driver.implicitly_wait(10)
     titles = driver.find_elements(By.TAG_NAME, 'h4')
     for title in titles:
         #print(title.text)
         if 'INGRESSO EXTRA PARA 2026.2' in title.text:
+            driver.quit()
             return True
+    driver.quit()
 
 def send_result(found):
     if found:
